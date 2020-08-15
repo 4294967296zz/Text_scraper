ee47910033196d8cb410bcf82e1ee7cc8a02534a
jyoghh <4294967296z@gmail.com>
2020-08-15 14:30:51 +0900

    0.2 - changed logo

--- script.js
+++ script.js
@@ -11,13 +11,6 @@ chrome.tabs.executeScript({
 	// alert(result[0]);
 	document.querySelector('#test_div').innerText = result[0];
 
-	// insta_btn click event - creates new chrome tab of my instagram
-	document.getElementById('insta_btn').onclick = function() {
-	    chrome.tabs.create({
-			url: "https://www.instagram.com/dooonnnnnng"
-		});
-	};
-
 });
 
 
